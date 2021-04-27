c639eba64f927f83900ba7687022ee124c28ae0d
Dueel <diego.bar@live.it>
2021-04-27 09:02:04 +0200

    nav & header

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix');
 mix
     .js('src/app.js', 'dist')
-    .sass('src/style.scss', '')
+    .sass('src/style.scss', '').options({processCssUrls: false})
     .setPublicPath('dist')
     .copy('src/index.html', 'dist')
 ;
